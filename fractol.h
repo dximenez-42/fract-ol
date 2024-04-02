121975445493d0634b4495442a2bb76370a609c1
Daniel Ximenez <danixidev@gmail.com>
2024-04-02 16:28:33 +0200

    Crear formas y hacer hook de ESC

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 11:34:36 by dximenez          #+#    #+#             */
-/*   Updated: 2024/04/02 14:49:33 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/04/02 16:18:49 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,36 @@
 # include "libft/libft.h"
 # include "mlx/mlx.h"
 # include <math.h>
+
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+enum Keycodes
+{
+	ESCAPE=53,
+	SPACE=49
+};
+
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
 
 #endif
